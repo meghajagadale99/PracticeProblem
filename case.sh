ba74c687b1f72eae931902e0eb1fa10e9df46821
Megha <meghajagadale99@gmail.com>
2021-08-03 15:17:24 +0530

    first commit

---
+++ case.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+echo "Enter  your lucky number"
+read n
+case $n in
+101)
+echo "you got 1st prize" ;;
+510)
+echo "you got 2nd price" ;;
+999)
+echo "you got 3rd prize" ;;
+*)
+echo "sorry, try for the next time" ;;
+esac
